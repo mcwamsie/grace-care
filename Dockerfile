01e481b71e1e716f1e441c17624b846a3c708335
Tinashe Makwarimba <48883645+mcwamsie@users.noreply.github.com>
2024-09-12 22:24:01 +0200

    Assembly Module

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 RUN mkdir -p /app/mediafiles
+RUN mkdir -p ./data
 
 # Collect static files
 RUN python manage.py collectstatic --noinput
--- core/settings.py
+++ core/settings.py
@@ -123,7 +123,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
+            #'NAME': 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
         }
     }
 
