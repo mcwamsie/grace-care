c52c44ebd44348ceeb3ab9ad59674451b06cbfaa
Tinashe Makwarimba <48883645+mcwamsie@users.noreply.github.com>
2024-09-13 15:15:48 +0200

    Payment Module

--- home/signals.py
+++ home/signals.py
@@ -13,15 +13,14 @@ from .utils import calculateContributions
 @receiver(post_save, sender=Member)
 def send_welcome_email_to_user(sender, instance, created, **kwargs):
     if created:  # Send the email only when the profile is created
-        church = instance.assembly.church
+        #church = instance.assembly.church
 
         # Generate a random password
-        random_password = random_password_generator(8)
+        #random_password = random_password_generator(8)
 
         # Set the password for the user
-        instance.set_password(random_password)
-        instance.save()
+        #instance.set_password(random_password)
+        #instance.save()
         calculateContributions(instance)
         # Send the welcome email with the random password
         #send_welcome_email(instance.email, instance.first_name, random_password, church)
-
